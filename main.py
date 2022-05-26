40799075dce98113414433efdfd581a68c9bd54f
Narayan Chowdhari <72499092+Narayan1801@users.noreply.github.com>
2022-05-26 15:04:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv.imread("group_face.jfif")
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+face_detect = face_cascade.detectMultiScale(gray_img, 1.1, 4)
+
+for (x, y, w, h) in face_detect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
+
+cv.imshow("Image", img)
+cv.waitKey(0)
